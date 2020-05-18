cc047e8644fb9d8c134e6b507b2cd85278bac01e
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-05-19 01:03:17 +0300

    Finish first version of Exercise3, debug it, but not comment.

--- Task 1/Exercise3/src/Days/Days.java
+++ Task 1/Exercise3/src/Days/Days.java
@@ -1,10 +1,45 @@
 package Days;
-import static java.lang.System.out;
-
 public class Days {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            var morning = new TwentyFourHours(new Time(8,05));
+            var day = new TwentyFourHours(new Time(11,00));
+            var evening = new TwentyFourHours(new Time(17,45));
+            var night0 = new TwentyFourHours(new Time(3, 00));
+            var night1 = new TwentyFourHours(new Time(21, 59));
+            var night2 = new TwentyFourHours(new Time(23, 59));
+            
+            System.out.println(morning.getPeriod().toString());
+            morning.printTime();
+            System.out.println(day.getPeriod().toString());
+            day.printTime();
+            System.out.println(evening.getPeriod().toString());
+            evening.printTime();
+            
+            System.out.println(night0.getPeriod().toString());
+            night0.printTime();
+            System.out.println(night1.getPeriod().toString());
+            night1.printTime();
+            System.out.println(night2.getPeriod().toString());
+            night2.printTime();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void CompareTest(Time t1, Time t2) {
+        switch(t1.compare(t2)) {
+            case 0 : 
+                System.out.println("Equals | " + t1.toString() + " == " + t2.toString()); 
+                break;
+            case 1 : 
+                System.out.println("Grater | " + t1.toString() + " > " + t2.toString()); 
+                break;
+            case -1 : 
+                System.out.println("Less | " + t1.toString() + " < " + t2.toString()); 
+                break;
+        }
     }
     
 }
---
+++ Task 1/Exercise3/src/Days/Program.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Days;
+
+/**
+ *
+ * @author andrey
+ */
+
+abstract class TimeItem {
+    protected TimeItem(int t) {this.t = t;}
+    private int t;
+    
+    public int get() {return t; }
+    public int compare(TimeItem item) {
+        if(item.t == t)
+            return 0;
+        return (t > item.t ? 1 : -1);
+    }
+    
+    @Override
+    public String toString() {
+        return Integer.toString(t);
+    }
+}
+
+class Hour extends TimeItem {
+    public Hour(int h) throws Exception {
+        super(h);
+        if(h < 0 || h > 23)
+            throw new Exception("Hours must be inside diaposone [0;23]");
+    }
+}
+
+class Minute extends TimeItem {
+    public Minute(int m) throws Exception {
+        super(m);
+        if(m < 0 || m > 59)
+            throw new Exception("Minute must be inside diaposone [0;23]");
+    }
+}
+
+class Time {
+    public Time(Hour h, Minute m){
+        this.h = h;
+        this.m = m;
+    }
+    public Time(int h, int m) {
+        try {
+            this.h = new Hour(h); 
+            this.m = new Minute(m);
+            
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+    
+    private TimeItem h, m;
+     
+    @Override
+    public String toString()
+    {
+        return h.toString() + ":" + (m.get() < 10 ? "0" : "") + m.toString();
+    }
+    
+    public int compare(Time t) {
+        int compare_h = h.compare(t.h);        
+        return (compare_h == 0 ? m.compare(t.m) : compare_h);
+    }
+}
+
+enum PeriodsOfTheDay {
+    Morning,
+    Day,
+    Evening,
+    Night
+}
+
+class TwentyFourHours {
+    public TwentyFourHours(Time t) {
+        this.t = t;
+    }
+    
+    private Time t;
+    
+    public void printTime() {
+        printTime("");
+    }
+    
+    public void printTime(String message) {
+        System.out.println(message + t.toString());
+    }
+    
+    public PeriodsOfTheDay getPeriod() {
+        return getPeriod(t);
+    }
+    
+    private static class PeriodsItem {
+        public PeriodsItem(Time up, Time low, PeriodsOfTheDay period) {
+            upBorder = up;
+            lowBorder = low;
+            this.period = period;
+        }
+        public final Time upBorder, lowBorder;
+        public final PeriodsOfTheDay period;
+    }
+            
+    static final PeriodsItem[] periods = new PeriodsItem[]{
+            new PeriodsItem(new Time(6,0), new Time(11,0), PeriodsOfTheDay.Morning),
+            new PeriodsItem(new Time(11,0), new Time(17,0), PeriodsOfTheDay.Day),
+            new PeriodsItem(new Time(17,0), new Time(22,0), PeriodsOfTheDay.Evening),
+            new PeriodsItem(new Time(22,0), new Time(6,0), PeriodsOfTheDay.Night)
+        };
+    
+    public static PeriodsOfTheDay getPeriod (Time t) {
+                
+        for (PeriodsItem period : periods) {
+            
+            boolean up = t.compare(period.upBorder) >= 0;
+            boolean down = t.compare(period.lowBorder) == -1;
+            
+            if((period.period == PeriodsOfTheDay.Night ? up || down : up && down))
+                return period.period;
+        }
+        
+        return null;
+    }
+}
