96c471a16f02878b57d390390b441a2c0fba66f9
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-05-19 20:48:49 +0300

    Add test file for Exercise1 and user interaction for Exercise1 and Exercise3

--- Task 1/Exercise1/src/Triangles/Triangles.java
+++ Task 1/Exercise1/src/Triangles/Triangles.java
@@ -1,36 +1,119 @@
 
 package Triangles;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Triangles {
 
-    public static void main(String[] args) {
+    private static ArrayList<Triangle> readData(String path, String delimeter, boolean isHeadder) throws IOException {
+        ArrayList<Triangle> data = new ArrayList<>();
+        var fileData = read(path);
         
-       Map<Angle,Point> m = new HashMap<>();
-       
-       m.put(Angle.A, new Point(1,5));
-       m.put(Angle.B, new Point(8,-3));
-       m.put(Angle.C, new Point(-3,-1));
-       
-       Triangle t = new Triangle(m);
-              
-       System.out.println("A" + t.getPoint(Angle.A));
-       System.out.println("B" + t.getPoint(Angle.B));
-       System.out.println("C" + t.getPoint(Angle.C));
-       
-      if(t.isRight())
-           System.out.println("Right");
-      
-      System.out.println("Aria is = " + Double.toString(t.getArea()));
-      
-      System.out.println("Angle A = " + t.getAngle(Angle.A));
-      System.out.println("Angle B = " + t.getAngle(Angle.B));
-      System.out.println("Angle C = " + t.getAngle(Angle.C));
-       
-      System.out.println("Angles summ = " +
-              (t.getAngle(Angle.A) + t.getAngle(Angle.B) + t.getAngle(Angle.C)));
+        if(fileData.size() < 1 || (fileData.size() < 2 && isHeadder))
+            throw new IOException("file doesn`t contains data");
+                 
+        for (int i = (isHeadder ? 1 : 0); i < fileData.size(); i++) {
+            var line = fileData.get(i).split(delimeter);
+            if(line.length != 6)
+                throw new IOException("File must containe 6 columns {A.x,A.y,B.x,B.y,C.x,C.y}");
+            Map<Angle,Point> m = new HashMap<>();
+            m.put(Angle.A, new Point(Integer.parseInt(line[0]),Integer.parseInt(line[1])));
+            m.put(Angle.B, new Point(Integer.parseInt(line[2]),Integer.parseInt(line[3])));
+            m.put(Angle.C, new Point(Integer.parseInt(line[4]),Integer.parseInt(line[5])));
+            
+            data.add(new Triangle(m));
+        }
+        
+        return data;
+    }
+    
+    private static ArrayList<String> read(String path) throws IOException {
+        
+        File file = new File(path);
+        
+        if(!file.exists())
+            throw new IOException("File doesn`t exists");
+        if(file.isDirectory())
+            throw new IOException("File expected");
+        
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader=null;
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            
+            String line;
+            while((line = reader.readLine()) != null)
+                list.add(line);            
+            
+        }catch(IOException ex){
+            throw ex;
+        }finally{
+            if(reader != null)
+                reader.close();
+        }
+                
+        return list;
+    }
+    
+    private static Angle getClosestPointToOX(Triangle t) {
+        Angle min = (Math.abs(t.getPoint(Angle.A).y) < Math.abs(t.getPoint(Angle.B).y) ? 
+                     Angle.A : Angle.B );
+
+        return (Math.abs(t.getPoint(min).y) < Math.abs(t.getPoint(Angle.C).y) ? min : Angle.C);
+    }
+    private static Angle getRightAngle(Triangle t) {
+        if(t.getAngle(Angle.A) == 90)
+            return Angle.A;
+        if(t.getAngle(Angle.B) == 90)
+            return Angle.B;
+        
+        return Angle.C;
+    }
+    
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        try {
+            System.out.print("Enter path to file : ");
+            String path = reader.readLine(); 
+            System.out.print("Enter delimeter : ");
+            String delimeter = reader.readLine();
+            System.out.print("Does file contains headders ? (Y/n) : ");
+            String headder = reader.readLine();
+            boolean isHeadders = headder.toLowerCase().compareTo("y") == 0 || 
+                                 headder.compareTo("") == 0;
+            
+            var triangles = readData(path,delimeter,isHeadders);
+            triangles.removeIf(x->!x.isRight());
+            
+            if(triangles.isEmpty()) {
+                System.out.println("Can`t find any right triangles");
+                return;
+            }
+            
+            triangles.forEach(x-> {
+                Angle closestPointToOX = getClosestPointToOX(x);
+                Angle rightAngle = getRightAngle(x);
+                
+                System.out.println("Right angle = " + rightAngle.toString() + 
+                                   x.getPoint(rightAngle).toString() + 
+                                   " | S = " + Double.toString(x.getArea()) + 
+                                   " | Closest to OX point = " + 
+                                   closestPointToOX.toString() + x.getPoint(closestPointToOX).toString());
+            });
+            
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Can`t convert to double " + e.getMessage());
+        }
     }
     
 }
--- Task 1/Exercise3/src/Days/Days.java
+++ Task 1/Exercise3/src/Days/Days.java
@@ -1,45 +1,28 @@
 package Days;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Days {
 
     public static void main(String[] args) {
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+         
         try {
-            var morning = new TwentyFourHours(new Time(8,05));
-            var day = new TwentyFourHours(new Time(11,00));
-            var evening = new TwentyFourHours(new Time(17,45));
-            var night0 = new TwentyFourHours(new Time(3, 00));
-            var night1 = new TwentyFourHours(new Time(21, 59));
-            var night2 = new TwentyFourHours(new Time(23, 59));
+            System.out.print("Enter hours : ");
+            int h = Integer.parseInt(reader.readLine());
             
-            System.out.println(morning.getPeriod().toString());
-            morning.printTime();
-            System.out.println(day.getPeriod().toString());
-            day.printTime();
-            System.out.println(evening.getPeriod().toString());
-            evening.printTime();
+            System.out.print("Enter Minutes : ");
+            int m = Integer.parseInt(reader.readLine());
             
-            System.out.println(night0.getPeriod().toString());
-            night0.printTime();
-            System.out.println(night1.getPeriod().toString());
-            night1.printTime();
-            System.out.println(night2.getPeriod().toString());
-            night2.printTime();
+            var time = new TwentyFourHours(new Time(h,m));
+            
+            time.printTime("Current time is : ");
+            System.out.println("Current period is " + time.getPeriod().toString());
+            
         } catch(Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    
-    public static void CompareTest(Time t1, Time t2) {
-        switch(t1.compare(t2)) {
-            case 0 : 
-                System.out.println("Equals | " + t1.toString() + " == " + t2.toString()); 
-                break;
-            case 1 : 
-                System.out.println("Grater | " + t1.toString() + " > " + t2.toString()); 
-                break;
-            case -1 : 
-                System.out.println("Less | " + t1.toString() + " < " + t2.toString()); 
-                break;
-        }
-    }
-    
 }
