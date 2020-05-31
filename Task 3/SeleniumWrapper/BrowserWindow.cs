36f0bc69fb55feb79346ad08099e8a84d79a15bd
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-06-01 01:49:54 +0300

    Implement class "BrowserWindow"

--- Task 3/SeleniumWrapper/BrowserWindow.cs
+++ Task 3/SeleniumWrapper/BrowserWindow.cs
@@ -10,66 +10,50 @@ namespace SeleniumWrapper
     {
         public BrowserWindow(IWebDriver webDriver)
         {
-            throw new NotImplementedException();
+            driver = webDriver;
         }
         
-        public string Url { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private readonly IWebDriver driver;
 
-        public string Title => throw new NotImplementedException();
-
-        public string Handle => throw new NotImplementedException();
-
-        public Point Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Size Size { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public void Back()
-        {
-            throw new NotImplementedException();
+        public string Url 
+        { 
+            get => driver.Url; 
+            set => Url = value; 
         }
 
-        public IWebElement FindElement(By by)
-        {
-            throw new NotImplementedException();
+        public string Title => driver.Title;
+
+        public string Handle => driver.CurrentWindowHandle;
+
+        public Point Position 
+        { 
+            get => driver.Manage().Window.Position; 
+            set => Position = value; 
+        }
+        public Size Size 
+        { 
+            get => driver.Manage().Window.Size; 
+            set => Size = value; 
         }
 
-        public ReadOnlyCollection<IWebElement> FindElements(By by)
-        {
-            throw new NotImplementedException();
-        }
+        public void Back() => driver.Navigate().Back(); 
 
-        public void Forward()
-        {
-            throw new NotImplementedException();
-        }
+        public IWebElement FindElement(By by) => driver.FindElement(by);
 
-        public void FullScreen()
-        {
-            throw new NotImplementedException();
-        }
+        public ReadOnlyCollection<IWebElement> FindElements(By by) => driver.FindElements(by);
 
-        public void GoToUrl(string url)
-        {
-            throw new NotImplementedException();
-        }
+        public void Forward() => driver.Navigate().Forward();
 
-        public void GoToUrl(Uri url)
-        {
-            throw new NotImplementedException();
-        }
+        public void FullScreen() => driver.Manage().Window.FullScreen();
 
-        public void Maximize()
-        {
-            throw new NotImplementedException();
-        }
+        public void GoToUrl(string url) => driver.Navigate().GoToUrl(url);
 
-        public void Minimize()
-        {
-            throw new NotImplementedException();
-        }
+        public void GoToUrl(Uri url) => driver.Navigate().GoToUrl(url);
 
-        public void Refresh()
-        {
-            throw new NotImplementedException();
-        }
+        public void Maximize() => driver.Manage().Window.Maximize();
+        
+        public void Minimize() => driver.Manage().Window.Minimize();
+
+        public void Refresh() => driver.Navigate().Refresh();
     }
 }
