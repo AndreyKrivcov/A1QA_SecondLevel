76aaad4df99cf27d09d0f699b3e0dcf40fca1849
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-06-02 13:13:07 +0300

    Add configuration object

--- Task 3/Config.cs
+++ Task 3/Config.cs
@@ -1,13 +1,59 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
 using SeleniumWrapper.BrowserFabrics;
 
 namespace Task_3
 {
+
+    [DataContract]
     class Config
     {
-        public BrowserType Browser { get; set; }
-        public string BrowserVersion { get; set; }
-        public string Url { get; set; }
+        public BrowserType Browser { get; set; } = BrowserType.Chrome;
+        [DataMember]
+        private string BrowserTypeName 
+        { 
+            get => Enum.GetName(typeof(BrowserType), Browser);
+            set 
+            {
+                BrowserType ans;
+                if(Enum.TryParse<BrowserType>(value,true, out ans))
+                {
+                    Browser = ans;
+                }
+            } 
+        }
+        [DataMember]
+        public string BrowserVersion { get; set; } = "Latest";
+        [DataMember]
+        public string Url { get; set; } = "https://market.yandex.ru";
+        [DataMember]
         public string Login { get; set; }
+        [DataMember]
         public string Passward { get; set; }
+
+        private static DataContractJsonSerializer serializer = 
+            new DataContractJsonSerializer(typeof(Config));
+        public void Serialize(string filePath)
+        {
+            using(var file = new FileStream(filePath,FileMode.Create))
+            {
+                serializer.WriteObject(file,this);
+            }
+        }
+        public static Config Deserialize(string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                return null;
+            }
+            
+            using(var file = new FileStream(filePath,FileMode.Open))
+            {
+                return (Config)serializer.ReadObject(file);
+            }
+        }
     }
 }
