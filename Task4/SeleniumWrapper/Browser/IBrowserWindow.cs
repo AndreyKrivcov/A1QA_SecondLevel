ee4faed4eb5325ce05d48c321275ea6569c15b5b
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-06-13 23:56:30 +0300

    A new methods inside Base element class was added.

--- Task4/SeleniumWrapper/Browser/IBrowserWindow.cs
+++ Task4/SeleniumWrapper/Browser/IBrowserWindow.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
 using SeleniumWrapper.Elements;
 
 namespace SeleniumWrapper.Browser
@@ -27,65 +23,4 @@ namespace SeleniumWrapper.Browser
         BaseElement FindElement(By by);
         ElementsKeeper FindElements(By by);
     } 
-
-    public class ElementsKeeper
-    {
-        public ElementsKeeper(IWebDriver driver, By by)
-        {
-            if(driver == null || by == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            this.driver = driver;
-            this.by = by;
-        }
-        public ElementsKeeper(IWebDriver driver, Func<IWebElement> element, By by) : this(driver, by)
-        {
-            if(element == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            this.element = element;
-        }
-
-        private readonly IWebDriver driver;
-        private readonly Func<IWebElement> element;
-        private readonly By by;
-
-        public static implicit operator ReadOnlyCollection<BaseElement>(ElementsKeeper collectionKeeper)
-        {
-            if(collectionKeeper.driver == null || collectionKeeper.by == null)
-                return null;    
-
-            ReadOnlyCollection<IWebElement> elementsFinder()
-            {
-                ISearchContext finder = (collectionKeeper.element == null ? collectionKeeper.driver : (ISearchContext)collectionKeeper.element());
-                return finder.FindElements(collectionKeeper.by);
-            }
-
-            var data = elementsFinder();
-
-            List<BaseElement> ans = new List<BaseElement>();
-            for (int i = 0; i < data.Count; i++)
-            {
-                int n = i;
-                ans.Add(new DefaultElement(()=>
-                {
-                    var items = elementsFinder();
-                    return (items.Count > n ? items[n] : null);
-                }, collectionKeeper.driver));
-            }
-
-            return ans.AsReadOnly();
-        }
-    }
-
-    internal class DefaultElement : BaseElement
-    {
-        public DefaultElement(Func<IWebElement> elementFinder, IWebDriver driver) : base(elementFinder,driver)
-        {
-        }
-    }
 }
--- Task4/SeleniumWrapper/Elements/BaseElement.cs
+++ Task4/SeleniumWrapper/Elements/BaseElement.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumWrapper.Elements
@@ -56,20 +60,99 @@ namespace SeleniumWrapper.Elements
         public bool Enabled => Element.Enabled;
         public void WaitForDisplayed(TimeSpan timeout, TimeSpan? sleepInterval = null)
         {
-            Wait(timeout, (IWebDriver x) => Displayed);
+            Wait(timeout, (IWebDriver x) => Displayed, sleepInterval, typeof(NoSuchElementException));
         }       
         public void WaitForExists(TimeSpan timeout, TimeSpan? sleepInterval = null)
         {
-            Wait(timeout, (IWebDriver x) => IsExists, sleepInterval);
+            Wait(timeout, (IWebDriver x) => IsExists, sleepInterval, typeof(NoSuchElementException));
         }
 
-        protected T Wait<T>(TimeSpan timeout, Func<IWebDriver, T> f, TimeSpan? sleepInterval = null)
+        protected T Wait<T>(TimeSpan timeout, Func<IWebDriver, T> f, TimeSpan? sleepInterval = null, params Type[] ignoringExceptions )
         {
             var wait = sleepInterval.HasValue ? new WebDriverWait(new SystemClock(),driver,timeout, sleepInterval.Value) 
                                               : new WebDriverWait(driver,timeout);
-            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+            if(ignoringExceptions != null && ignoringExceptions.Count() > 0)
+            {
+                wait.IgnoreExceptionTypes(ignoringExceptions);
+            }
 
             return wait.Until(x=> f(x));
         } 
+
+        public BaseElement FindElement(By by) => new DefaultElement(()=>driver.FindElement(by),driver);
+        public ElementsKeeper FindElements(By by) => new ElementsKeeper(driver, elementFinder, by);
+
+        public void Click()
+        {
+            Element.Click();
+        }
+
+        public void DoubleClick()
+        {
+            Actions act = new Actions(driver);
+            act.DoubleClick(Element);
+        }
+    }
+
+    public class ElementsKeeper
+    {
+        public ElementsKeeper(IWebDriver driver, By by)
+        {
+            if(driver == null || by == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.driver = driver;
+            this.by = by;
+        }
+        public ElementsKeeper(IWebDriver driver, Func<IWebElement> element, By by) : this(driver, by)
+        {
+            if(element == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.element = element;
+        }
+
+        private readonly IWebDriver driver;
+        private readonly Func<IWebElement> element;
+        private readonly By by;
+
+        public static implicit operator ReadOnlyCollection<BaseElement>(ElementsKeeper collectionKeeper)
+        {
+            if(collectionKeeper.driver == null || collectionKeeper.by == null)
+                return null;    
+
+            ReadOnlyCollection<IWebElement> elementsFinder()
+            {
+                ISearchContext finder = (collectionKeeper.element == null ? collectionKeeper.driver : (ISearchContext)collectionKeeper.element());
+                return finder.FindElements(collectionKeeper.by);
+            }
+
+            var data = elementsFinder();
+
+            List<BaseElement> ans = new List<BaseElement>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                int n = i;
+                ans.Add(new DefaultElement(()=>
+                {
+                    var items = elementsFinder();
+                    return (items.Count > n ? items[n] : null);
+                }, collectionKeeper.driver));
+            }
+
+            return ans.AsReadOnly();
+        }
+    }
+
+    internal class DefaultElement : BaseElement
+    {
+        public DefaultElement(Func<IWebElement> elementFinder, IWebDriver driver) : base(elementFinder,driver)
+        {
+        }
     }
 }
