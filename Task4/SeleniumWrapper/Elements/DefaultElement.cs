03d25d8b3677805308c0e7953bc8d717a8f06c27
AndreyKrivcov <Andrey.Krivcov@gmail.com>
2020-06-25 13:48:43 +0300

    Move DefaultElement into separate file

---
+++ Task4/SeleniumWrapper/Elements/DefaultElement.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using SeleniumWrapper.Browser;
+
+namespace SeleniumWrapper.Elements
+{
+    internal sealed class DefaultElement<T> : BaseElement where T : BaseElement
+    {
+        public DefaultElement(WebElementKeeper elementKeeper) : base(elementKeeper)
+        {
+        }
+
+        public static implicit operator T(DefaultElement<T> element)
+        {
+            return (T)Activator.CreateInstance(typeof(T),element.Element);
+        }
+    }
+
+    internal static class ElementCollectionExtention
+    {
+        public static List<T> ToElementArray<T>(this IEnumerable<DefaultElement<T>> collection) where T : BaseElement
+        {
+            List<T> ans = new List<T>();
+            foreach (var item in collection)
+            {
+                ans.Add(item);
+            }
+
+            return ans;
+        }
+
+        public static List<T> ToElementArray<T>(this IEnumerable<IWebElement> collection, Func<int,T> outputElementCreator)
+        {
+            List<T> ans = new List<T>();
+            for (int i = 0; i < collection.Count(); i++)
+            {
+                ans.Add(outputElementCreator(i));
+            }
+
+            return ans;
+        }
+    }
+}
